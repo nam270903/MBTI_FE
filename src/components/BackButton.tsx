136c4942f8f36aa2bdca4bef395e8ad794f9b92f
nam270903 <nam270903@gmail.com>
2025-05-26 17:22:07 +0700

    FIX: replace Back Button from text to icon

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const BackButton = ({ label = "<" }) => {
   const navigation = useNavigation();
-  
+  const back = require('../assets/icon/back.png');
+
   return (
-    <TouchableOpacity 
-      style={styles.button} 
-      onPress={() => navigation.goBack()}
-      activeOpacity={0.7}>
-      <Text style={styles.text}>{label}</Text>
+    <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+      <Image source={back}/>
     </TouchableOpacity>
   );
 };
@@ -21,10 +19,6 @@ const styles = StyleSheet.create({
     width: 20,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-    color: '#404040',
   },
 });
 
